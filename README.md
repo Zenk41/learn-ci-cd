821af76d28f2eee9ff03d18c2daed567d2f3f759
Zenk41 <ardhidhani@gmail.com>
2022-10-21 22:51:39 +0700

    Relaunch the Instance

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-TEST
-TEST2
-TEST3
-TEST4
-TEST5
-TEST6
-TEST7
+Relaunch the instances
--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-go test -v ./businesses/users --race -coverprofile=./coverage/profile.out -covermode=atomic
+go test -v ./businesses/... --race -coverprofile=./coverage/profile.out -covermode=atomic
 go tool cover -html=./coverage/profile.out -o ./coverage/coverage.html
